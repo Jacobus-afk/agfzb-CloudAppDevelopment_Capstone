0014b96173bb68ccf6b7aacb60787884d332cad7
Jacobus-afk <62353676+Jacobus-afk@users.noreply.github.com>
2022-03-27 15:30:56 +0200

    Update get_dealerships.js

--- functions/sample/nodejs/get_dealerships.js
+++ functions/sample/nodejs/get_dealerships.js
@@ -2,33 +2,35 @@
  * Get all dealerships
  */
 
- const Cloudant = require('@cloudant/cloudant');
+const Cloudant = require('@cloudant/cloudant');
+const openwhisk = require('openwhisk');
+const ow = openwhisk();
 
-
- async function main(params) {
+async function main(params) {
      
-    const secret = {
-        COUCH_URL: "https://apikey-v2-1zu36jhjj3wlcqelugj4ulumbmorzckdvlbqfbmwvamk:ccaa2118d9eb704bd6c1ac03bc594cdd@d1548402-28e1-4f4a-a948-37ebd76a8d29-bluemix.cloudantnosqldb.appdomain.cloud",
-        IAM_API_KEY: "NubmTUUUcpsViZ8RU0li4GzxgXoNMplV0UHILcExvQC5",
-        COUCH_USERNAME: "apikey-v2-1zu36jhjj3wlcqelugj4ulumbmorzckdvlbqfbmwvamk"
-    };
+    const secret = await ow.actions.invoke({ 
+        name: 'dealerships/retrieve_credentials',
+        blocking: true,
+        result: true,
+        params: {}
+    })
      
-     const cloudant = Cloudant({
-         url: secret.COUCH_URL,
-         plugins: { iamauth: { iamApiKey: secret.IAM_API_KEY } }
-     });
- 
+    const cloudant = Cloudant({
+        url: secret.COUCH_URL,
+        plugins: { iamauth: { iamApiKey: secret.IAM_API_KEY } }
+    });
+    
     let status_code = 200;
     let headers = { 'Content-Type': 'application/json' };
     let resp = '';
- 
-    const dealershipDB = await cloudant.use('dealerships');
- 
+    
+    // const dealershipDB = await cloudant.use('dealerships');
+    
     if (params.state) {
         try {
-            let resp = await dealershipDb.find({
+            resp = await cloudant.use('dealerships').find({
                 "selector": {
-                    "state": {
+                    "st": {
                         "$eq": params.state
                     }
                 }
@@ -46,10 +48,10 @@
             return { error: error.description } ;
         }
     }
- 
+    
     return {
         statusCode: status_code,
         headers: headers,
         body: resp
     }
- }
+}
