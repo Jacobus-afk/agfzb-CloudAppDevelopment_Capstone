77d87296547081f056991575c399d188ecd61217
Jacobus-afk <62353676+Jacobus-afk@users.noreply.github.com>
2022-03-27 18:17:15 +0200

    Update retrieve_credentials.js

--- functions/sample/nodejs/retrieve_credentials.js
+++ functions/sample/nodejs/retrieve_credentials.js
@@ -1,16 +1,7 @@
-/**
-  *
-  * main() will be run when you invoke this action
-  *
-  * @param Cloud Functions actions accept a single parameter, which must be a JSON object.
-  *
-  * @return The output of this action, which must be a JSON object.
-  *
-  */
 function main(params) {
-	return {
-	    COUCH_URL: "",
-        IAM_API_KEY: "",
-        COUCH_USERNAME: ""
-	};
+    params.COUCH_URL = "https://apikey-v2-1zu36jhjj3wlcqelugj4ulumbmorzckdvlbqfbmwvamk:ccaa2118d9eb704bd6c1ac03bc594cdd@d1548402-28e1-4f4a-a948-37ebd76a8d29-bluemix.cloudantnosqldb.appdomain.cloud",
+    params.IAM_API_KEY = "NubmTUUUcpsViZ8RU0li4GzxgXoNMplV0UHILcExvQC5",
+    params.COUCH_USERNAME = "apikey-v2-1zu36jhjj3wlcqelugj4ulumbmorzckdvlbqfbmwvamk"
+
+    return params;
 }
